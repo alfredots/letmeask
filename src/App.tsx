42af6117b7cdd4aa35ab3399b991c6d885b1ea77
alfredots <tito.alfred@hotmail.com>
2021-06-22 11:39:13 -0300

    segunda aula do nlw concluida

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { AuthContextProvider } from './contexts/AuthContext'
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
+
+import './styles/global.scss'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
