cb8d3e639b19a3419a99418dbc202163697ad2e1
alfredo <alfredtito97@gmail.com>
2021-06-25 10:01:49 -0300

    adicionado eslint e prettier

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,18 @@ import { Room } from './pages/Room'
 import './styles/global.scss'
 
 function App() {
-
   return (
     <BrowserRouter>
       <AuthContextProvider>
         <Switch>
-          <Route path="/" exact component={Home}/>
+          <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
           <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
