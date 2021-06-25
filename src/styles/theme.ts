7980e6b651ec95267a22ffbea01b6f9ac70ed835
alfredo <alfredtito97@gmail.com>
2021-06-25 14:15:32 -0300

    iniciando o uso de styled components

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+export default {
+  colors: {
+    black: '#29292E',
+    shadow: '#050206',
+    purple: '#835AFD',
+    gradient: 'linear-gradient(139.44deg, #485BFF 0%, #FF59F8 96.19%)',
+    gray: {
+      dark: '#737380',
+      medium: '#A8A8B3',
+      light: '#DBDCDD'
+    },
+    white: {
+      background: '#F8F8F8',
+      details: '#FEFEFE'
+    },
+    pink: {
+      dark: '#E559F9',
+      light: '#D67EE2'
+    },
+    hover: {
+      hover: '#6F4BD8',
+      danger: '#D73754',
+      grayMedium: '#7E7E86',
+      grayLight: '#CECECE'
+    }
+  }
+}
